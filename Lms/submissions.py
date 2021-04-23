23ef01de323067cd23f4de20f4a8b847f37c3bb2
Zalak <zalakbhojani@yahoo.com>
2021-04-23 10:31:07 +0530

    added submission-lms

---
+++ Lms/submissions.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import requests, time, os
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+def submission(url : str, filePath: str) -> {} : # needs url of submission and file path
+    driver.get(url)
+    time.sleep(3)
+    button = driver.find_element_by_css_selector('div.fp-btn-add')
+    actions = ActionChains(driver)
+    actions.click(button)
+    actions.perform()
+    time.sleep(3)
+    chooseFileBtn = driver.find_element_by_name("repo_upload_file")
+    chooseFileBtn.send_keys(os.path.abspath(filePath))
+    # chooseFileBtn.send_keys(Keys.RETURN)
+    chooseFileBtn = driver.find_element_by_css_selector('button.fp-upload-btn')
+    actions.click(chooseFileBtn)
+    actions.perform()
+    submitBtn = driver.find_element_by_css_selector('input#id_submitbutton')
+    actions.click(submitBtn)
+    actions.perform()
+    
